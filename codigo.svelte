f0c11ffe6bd450be7ec7ac0b3b0aed29f5010513
boboslz <61566910+boboslz@users.noreply.github.com>
2020-04-21 13:19:16 -0300

    Add files via upload

---
+++ codigo.svelte
@@ -0,0 +1,33 @@
+<script>
+	let h = new Date();
+	let horas = h.getHours();
+
+</script>
+
+{#if horas < 5}
+		<h1>Boa noite!!!</h1>
+{:else}
+	{#if horas < 8}
+		<h1>Bom Dia!!!</h1>
+{:else}
+	{#if horas < 12}
+		<h1>Bom Dia!!!</h1>
+{:else}
+	{#if horas < 18}
+		<h1>Boa Tarde!!!</h1>
+{:else}
+	<h1>Boa Noite!!!</h1>
+			{/if}
+		{/if}
+	{/if}
+{/if}
+
+<style>
+h1{
+	text-align: center;
+	color: #ff3e00;
+	text-transform: uppercase;
+	font-size: 4em;
+	font-weight: 100;
+}
+</style>
